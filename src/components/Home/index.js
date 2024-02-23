d265a175034074634ee71c5b7e10a1c75d19d852
RishithaVanamala <rishithavanamala@gmail.com>
2024-02-23 17:39:50 -0500

    Home page logo and animation are added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,56 @@
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['i', 's', 'h', 'i', 't', 'h', 'a']
+    const jobArray = ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //         setLetterClass('text-animate-hover')
+    //     }, 4000)
+    // }, [])
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+    
+        // Cleanup function to clear the timeout
+        return () => clearTimeout(timer);
+    }, []);
+    
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
-                <img src={LogoTitle} alt="developer" />
-                ishitha
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
                 <br />
-                Student 
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+                <img src={LogoTitle} alt="developer" />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15} />
+                <br />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22} />
                 </h1>
                 <h2>
                     Master's Student at Syracuse University
                 </h2>
                 <Link to='/contact' className='flat-button'>CONTACT ME</Link>
-            </div>            
+            </div>  
+            <Logo />          
         </div>
     );
 }
