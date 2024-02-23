c8d649cbd82d91c4ec39701242affd2153805385
RishithaVanamala <rishithavanamala@gmail.com>
2024-02-23 17:46:27 -0500

    Animation change

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -14,18 +14,30 @@ const Logo = () => {
     useEffect(() => {
         gsap.registerPlugin(DrawSVGPlugin)
 
-        gsap.timeline()
+        gsap
+        .timeline()
         .to(bgRef.current, {
-            duration: 1,
-            opacity: 1,
+          duration: 1,
+          opacity: 1,
         })
-
         .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20,
+          drawSVG: 0,
+          duration: 20,
         })
+  
+      gsap.fromTo(
+        solidLogoRef.current,
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          delay: 4,
+          duration: 4,
+        }
+      )
     }, [])
-
+    
     return (
         <div className='logo-container' ref={bgRef}>
             <img ref={solidLogoRef} className='solid-logo' src={LogoS} alt="R"/>
